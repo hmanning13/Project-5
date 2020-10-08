97554378cadd2647beb5a8ad7a821d173c1bb8c4
Hayden Manning <67125970+hmanning13@users.noreply.github.com>
2020-10-08 16:07:41 -0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -84,19 +84,27 @@ function generateGallery(data) {
 };
 
 
-/*(function generateOptions(data) {
-    const options = data.map(item => `
-        <option value="${item}">${item}</option>
-    `).join("");
-    select.innerHTML = options;
-}
-*/
+function generateInfo(employee, index) {
+    const modalHTML = `
+       <div class="modal-container">
+          <div class="modal">
+             <button type="button" id="modal-close-btn" class="modal-close-btn">X</button>
+             <div class="modal-info-container">
+                <img class="modal-img" src="${employee.picture.large}" alt="profile-picture">
+                <h3 id="name" class="modal-name cap">${employee.name.first} ${employee.name.last}</h3>
+                <p class= "modal-text">${employee.email}</p>
+                <p class= "modal-text cap">${employee.location.city}</p>
+                <p class="modal-text">${employee.cell.replace} </p>
+                <p class= "modal-text">${employee.location.street.number} ${employee.location.street.name} ${employee.location.city}, ${employee.location.state} ${employee.loaction.zip}</p>
+                <p class= "modal-text">Birthday: ${employee.dob.date.slice(5,7)}/${employee.dob.date.slice(8,10)}/${employee.dob.date.slice(2,4)}</p>
+            </div>
+        </div>
 
-/*function generateImages(data) {
-    const html = `
-        <img src="${data}" alt>
-        <p>Click to learn more about ${select.value}</p>
+        <div class="modal-btn-container">
+            <button type="button" id="modal-prev" class= "modal-prev btn">Prev</button>
+            <button type= "button" id= "modal-next" class= "modal-next btn">Next</button>
+        </div>
+      </div>
     `;
-    card.innerHTML = html;
+    body.insertAdjacentHTML("beforeend", modalHTML)
 }
-*/
